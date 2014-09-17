66a49b88064f0354696cd612dcc94f75a3e00306
Andreas Pehrson <pehrson@comoyo.com>
2014-09-17 23:25:50 +0200

    Fix build errors caused by the silence-logging patch

--- profiling/patches/all-scx15_sp7715ga/vendor/sprd/open-source/silence_verbose_logging.sh
+++ profiling/patches/all-scx15_sp7715ga/vendor/sprd/open-source/silence_verbose_logging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for FILE in $(find libs -type f); do
-    sed -ni '1h;1!H;${g;s/\n\([ 	]*ALOG[A-Z_]*([^;]*);\)/\n((void)0);\n#if 0\n\1\n#endif/g;s/\(define[^a-zA-Z_]*\)\([a-zA-Z_]*\)[^\n]*ALOG[A-Z][^\n]*\n/\1\2(...) ((void)0)\n/g;p}' "$FILE"
+    sed -ni '1h;1!H;${g;s/\n\([ 	]*ALOG[A-Z_]*([^;]*);\)/\n((void)0);\n#if 0\n\1\n#endif/g;s/\(define[^a-zA-Z_]*\)\([a-zA-Z_]\+\)[^\n]*ALOG[A-Z][^\n]*\n/\1\2(...) ((void)0)\n/g;p}' "$FILE"
 done
